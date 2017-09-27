45d22cd459730e8cfbfb5264967e480105cbcb90
Xinzhou Liu <xl6v8@mail.missouri.edu>
2017-09-27 15:39:11 -0500

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -486,6 +486,14 @@ TEST (load_process_control_blocks, nullFilePath) {
     score+=5;
 }
 
+TEST (load_process_control_blocks, noFormatFile) {
+
+    dyn_array_t* da = load_process_control_blocks ("noFORMAT");
+    ASSERT_EQ(da,(dyn_array_t*)NULL);
+
+    score+=10;
+}
+
 TEST (load_process_control_blocks, notFoundFile) {
 
     dyn_array_t* da = load_process_control_blocks ("NotARealFile.Awesome");
@@ -522,6 +530,22 @@ TEST (load_process_control_blocks, incorrectPCBFoundFile) {
     score+=5;
 }
 
+TEST (load_process_control_blocks, overSizedPCBFoundFile) {
+    const char* fname = "OVERFLOW.RULES";
+    uint32_t pcb_num = 3;
+    uint32_t pcbs[5][2] = {{1,1},{2,2},{3,3},{4,4},{5,5}};
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
+    int flags = O_CREAT | O_TRUNC | O_WRONLY;
+    int fd = open(fname, flags, mode);
+    write(fd,&pcb_num,sizeof(uint32_t));
+    write(fd,pcbs,5 * sizeof(uint32_t)*2);
+    close(fd);
+    dyn_array_t* da = load_process_control_blocks (fname);
+    ASSERT_EQ(da,(dyn_array_t*)NULL);
+
+    score+=10;
+}
+
 TEST (load_process_control_blocks, fullFoundFile) {
     const char* fname = "../test/PCBs.bin";
     uint32_t pcb_num = NUM_PCB;
@@ -547,6 +571,33 @@ TEST (load_process_control_blocks, fullFoundFile) {
 
     score+=10;
 }
+
+TEST (load_process_control_blocks, anotherFullFoundFile) {
+    const char* fname = "../test/anotherPCBs.bin";
+    uint32_t pcb_num = NUM_PCB;
+    uint32_t pcbs[NUM_PCB][2];
+    for (uint32_t p = 0; p < pcb_num; ++p) {
+        pcbs[p][0] = rand() % 10 + 5;
+        pcbs[p][1] = p;
+    }
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP;
+    int flags = O_CREAT | O_TRUNC | O_WRONLY;
+    int fd = open(fname, flags, mode);
+    write(fd,&pcb_num,sizeof(uint32_t));
+    write(fd,pcbs,pcb_num * sizeof(uint32_t)*2);
+    close(fd);
+    dyn_array_t* da = load_process_control_blocks (fname);
+    ASSERT_NE(da, (dyn_array_t*) NULL);
+    for (size_t i = 0; i < dyn_array_size(da); ++i) {
+        ProcessControlBlock_t * pPCB = (ProcessControlBlock_t *)dyn_array_at(da, i);
+        ASSERT_EQ(pPCB->remaining_burst_time, pcbs[i][0]);
+        ASSERT_EQ(pPCB->priority, pcbs[i][1]);
+    }
+    dyn_array_destroy(da);
+
+    score+=10;
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
